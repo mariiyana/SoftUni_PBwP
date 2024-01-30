c1cf9ec15ce05d4f0ca814b6aaa1e89029d0aba6
mariiyana <mariyanageorgieva00@gmail.com>
2024-01-30 19:50:03 +0100

    First code from git

--- SoftUni-exercises/08_pet_shop.py
+++
@@ -1,4 +0,0 @@
-dog_food = float(input())
-cat_food = int(input())
-final_price = float(dog_food * 2.50) + int(cat_food * 4)
-print(f"{final_price} lv.")
--- SoftUni-exercises/09_yard_greening.py
+++
@@ -1,6 +0,0 @@
-area = float(input())
-price = area * 7.61
-price_percentage = price * 0.18
-final_price = price - price_percentage
-print(f"The final price is: {final_price} lv.")
-print(f"The discount is: {price_percentage} lv.")
